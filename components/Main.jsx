ced0e8713107548e142118a70aba27d3e246e184
UAsad99 <usasad99@gmail.com>
2022-05-15 22:20:43 +0000

    added some changes to Main Banner

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
     <div className="custom-banner text-white">
       <Header />
       <div className="lg:container flex flex-col lg:flex-row mx-auto pt-32 -mb-3 px-6 sm:px-10 lg:pt-[280px] lg:pb-[250px] px-10">
-        <div className="lg:w-[52.544%] md:pr-40 lg:pr-16 pb-12 lg:pb-0">
+        <div className="lg:w-[52.544%] md:pr-40 lg:pr-16 lg:pb-0">
           <div className="mb-5 flex items-center">
             <span className={`w-3 h-3 main_bg rounded-full`} />
             <p className="pl-3 text-[13px] font-rubik font-medium tracking-[3px]">{data.main_banner.small_title}</p>
--- components/Video.jsx
+++ components/Video.jsx
@@ -1,9 +1,10 @@
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import data from "data/main.json";
 import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
 export default function Video() {
   const [playVideo, setPlayVideo] = useState(false);
+  const [videoSize, setVideoSize] = useState(0);
   const videoRef = useRef(null);
 
   const switchVideo = () => {
@@ -16,23 +17,35 @@ export default function Video() {
     }
   }
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener('resize', () => {
+        setVideoSize({height: videoRef.current.offsetHeight, width: videoRef.current.offsetWidth})
+      });
+    }
+  }, []);
+        console.log(videoRef.current)
+
   return (
-    <div className="h-full flex justify-center items-center relative">
-      <video
-        src={data.main_banner.video.link}
-        type="video/mp4"
-        ref={videoRef}
-        className="group rounded-[16px] w-full"
-      >Your Browser Cannot Support this video please try with another browser</video>
-      <button
-        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[23px] md:text-[38px] hover:text-black customTransition w-12 h-12 md:w-16 md:h-16 main_bg rounded-full flex justify-center items-center ${playVideo ? "opacity-40" : ""}`}
-        onClick={switchVideo}
-      >
-        {playVideo
-          ? <BsPauseFill />
-          : <BsFillPlayFill />
-        }
-      </button>
+    <div className="flex justify-center items-center relative">
+      <div style={{width: videoSize.width, height: videoSize.height}} />
+      <div className="w-full absolute top-[50px]">
+        <video
+          src={data.main_banner.video.link}
+          type="video/mp4"
+          ref={videoRef}
+          className="group rounded-[16px] w-full"
+        >Your Browser Cannot Support this video please try with another browser</video>
+        <button
+          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[23px] md:text-[38px] hover:text-black customTransition w-12 h-12 md:w-16 md:h-16 main_bg rounded-full flex justify-center items-center ${playVideo ? "opacity-40" : ""}`}
+          onClick={switchVideo}
+        >
+          {playVideo
+            ? <BsPauseFill />
+            : <BsFillPlayFill />
+          }
+        </button>
+      </div>
     </div>
   );
 }
