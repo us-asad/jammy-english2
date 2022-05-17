4b85a5b7c8c474a0e16bdd6ab249848de2afd777
UAsad99 <usasad99@gmail.com>
2022-05-17 00:57:52 +0000

    fixed svg bugs

--- components/ContactDetails.jsx
+++ components/ContactDetails.jsx
@@ -1,4 +1,5 @@
 import data from "data/contact.json";
+import Image from "next/image";
 import { FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { Facebook, Instagram, Twitter, Youtube } from "components/Svgs";
@@ -6,8 +7,8 @@ import { Facebook, Instagram, Twitter, Youtube } from "components/Svgs";
 export default function ContactDetails() {
   return (
     <div className="px-6 py-10 md:p-14 md:pr-16">
-      {data.contact_details.map(item => (
-        <div className="mb-10 md:mb-5 pb-5 border-b-[1px] border-black flex flex-col sm:flex-row">
+      {data.contact_details.map((item, i) => (
+        <div key={`contact_detail_${i}`} className="mb-10 md:mb-5 pb-5 border-b-[1px] border-black flex flex-col sm:flex-row">
           <div className="flex justify-center items-center w-[52px] h-[52px] border-2 mb-2 sm:mb-0 sm:mr-4 text-[16px] border-[#1579e6] rounded-full text-[#1579e6]">
             {item.icon === "phone" ? <FaPhone /> : <MdEmail />}
           </div>
@@ -22,23 +23,19 @@ export default function ContactDetails() {
       </h4>
       <div className="flex space-x-4 md:space-x-3 justify-center md:justify-start">
         {data.social_icons.map(icon => (
-          <a
+          <a 
+            key={icon.link}
             href={icon.link}
             rel="noreferrer"
             target="_blank"
             className="hover:scale-105 hover:drop-shadow-lg customTransition"
           >
-            {
-              icon.name.toLowerCase() === "facebook"
-              ? <Facebook />
-              : icon.name.toLowerCase() === "instagram"
-              ? <Instagram />
-              : icon.name.toLowerCase() === "twitter"
-              ? <Twitter />
-              : icon.name.toLowerCase() === "youtube"
-              ? <Youtube />
-              : ""
-            }
+            <Image
+              src={icon.icon.link}
+              alt={icon.icon.alt}
+              width={45}
+              height={45}
+            />
           </a>
         ))}
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "styles/globals.css";
 
 function App({ Component, pageProps }) {
   return (
-    <main className="overflow-x-hidden min-w-screen min-h-screen selection:bg-blue-500 selection:text-white">
+    <main className="overflow-x-hidden min-w-screen min-h-screen selection:bg-[#f7b500] selection:text-white">
       <Component {...pageProps} />
     </main>
   );
