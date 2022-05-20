9b56551e18cecc51192e656d09d849ba4c05af29
UAsad99 <usasad99@gmail.com>
2022-05-20 06:45:34 +0000

    overflow bug fixed at LessonSideBar

--- containers/LessonSideBar.jsx
+++ containers/LessonSideBar.jsx
@@ -1,12 +1,9 @@
-import { useState, useRef } from "react";
 import data from "data/courses.json";
 import Link from "next/link";
 import { MdOutlineArrowBackIosNew, MdPlayLesson } from "react-icons/md";
 import { BsBookHalf } from "react-icons/bs";
 
 export default function LessonSideBar({ switchShowSB, showSideBar }) {
-  const headerRef = useRef(null);
-
   return (
     <div className={`w-[349px] small-mobile-w-full bg-white border-r-[#e2e7ed] border-r h-full z-10 fixed responsive-transition ${showSideBar ? "left-0" : "-left-[120%] md:-left-[280px]"}`}>
       <span
@@ -15,16 +12,13 @@ export default function LessonSideBar({ switchShowSB, showSideBar }) {
       >
         <MdOutlineArrowBackIosNew className="text-[24px] text-white font-semibold" />
       </span>
-      <h3
-        className="flex items-center space-x-2.5 px-[18px] py-[13px] bg-blue-500 font-poppins font-bold text-[18px] text-white"
-        ref={headerRef}
-      >
-        <BsBookHalf />
-        <span>{data.name}</span>
+      <h3 className="flex items-center space-x-2.5 px-[18px] py-[13px] bg-blue-500 font-poppins font-bold text-[18px] text-white">
+        <BsBookHalf className="min-w-[18px] min-h-[18px]" />
+        <span className="line-clamp-1">{data.name}</span>
       </h3>
       <div
         className={`max-h-full ${showSideBar ? "overflow-y-auto" : "overflow-hidden"}`}
-        style={{maxHeight: `calc(100% - ${!headerRef.current?.offsetHeight && "53"}px)`}}
+        style={{maxHeight: `calc(100% - 53px)`}}
       >
       {data.lessons.map((lesson, i) => (
         <Link key={i} href={`/${data.slug}/lessons/${lesson.slug.link}`}>
