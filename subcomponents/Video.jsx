2c9bfa08809cb29dd2274aae9e27fad6d4434415
Asad <usasad99@gmail.com>
2022-06-15 01:45:53 +0500

    some changes added #3

--- subcomponents/Video.jsx
+++ subcomponents/Video.jsx
@@ -33,11 +33,12 @@ export default function Video({ video }) {
           className="rounded-[16px] w-full"
           autoPlay="autoplay"
         >Your Browser Cannot Support this video please try with another browser</video>
-        {!playVideo && <span className="animate-ping absolute top-[46.5%] z-10 left-[48.2%] text-[23px] md:text-[38px] text-white customTransition w-12 h-12 md:w-[50px] md:h-[50px] bg-main rounded-full"></span>}
         <button
           className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[11] text-[23px] md:text-[38px] text-white customTransition w-12 h-12 md:w-16 md:h-16 bg-main rounded-full flex justify-center items-center hover:opacity-100 ${playVideo ? "opacity-0 group-hover:opacity-40" : ""}`}
           onClick={switchVideo}
         >
+        {!playVideo && <span className="animate-ping absolute z-10 left-[7px] md:left-[8px] text-0 md:text-[38px] text-white customTransition w-[35px] h-[35px] md:w-[50px] md:h-[50px] bg-main rounded-full"></span>}
+
           {playVideo
             ? <BsPauseFill />
             : <BsFillPlayFill />
