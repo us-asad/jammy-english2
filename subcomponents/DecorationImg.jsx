7e2255589664a9460bbe0f1d12326cde7d7c54b4
Asad <usasad99@gmail.com>
2022-06-16 01:00:46 +0500

    added some changes #3

--- subcomponents/DecorationImg.jsx
+++ subcomponents/DecorationImg.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function DecorationImg({ img, className, style, dataAos }) {
   return (
     <div data-aos={dataAos} style={style} className={`hidden xl:block absolute z-[0] ${className}`}>
-      <Image
+      <img
         src={img?.src}
         alt="decoration shape"
         width={img?.width}
